32b4f2b8d8521b0a078d4cdf8f6e122c3fc1b7b5
maggieoneill <buttmunch2000>
2015-10-21 15:37:50 -0700

    getters and setters in BandMember class

--- app/src/main/java/com/example/guest/fansite/models/BandMember.java
+++ app/src/main/java/com/example/guest/fansite/models/BandMember.java
@@ -4,4 +4,37 @@ package com.example.guest.fansite.models;
  * Created by Guest on 10/21/15.
  */
 public class BandMember {
+    private String mName;
+    private String mDescription;
+    private int mImage;
+
+    public BandMember(String name, String description, int image){
+        mName = name;
+        mDescription = description;
+        mImage = image;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public int getImage() {
+        return mImage;
+    }
+
+    public void setImage(int image) {
+        mImage = image;
+    }
 }

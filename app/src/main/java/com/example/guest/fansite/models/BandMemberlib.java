a8ad5391a5c4a2583cd1c67e0d4aed5f7bf7f5c3
maggieoneill <margaretanneoneill@gmail.com>
2015-10-22 10:29:03 -0700

    create array list for band members.

--- app/src/main/java/com/example/guest/fansite/models/BandMemberlib.java
+++ app/src/main/java/com/example/guest/fansite/models/BandMemberlib.java
@@ -1,7 +1,51 @@
 package com.example.guest.fansite.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by Guest on 10/21/15.
  */
 public class BandMemberlib {
+
+    private ArrayList<BandMember> mBandMembers;
+
+    public BandMemberLib(){
+        setBandMembers()
+    }
+
+    private void setBandMembers() {
+        mBandMembers = new ArrayList<>();
+
+        mBandMembers.add(new BandMember(
+
+
+        ));
+
+        mBandMembers.add(new BandMember(
+
+
+        ));
+
+        mBandMembers.add(new BandMember(
+
+
+        ));
+
+        mBandMembers.add(new BandMember(
+
+
+        ));
+
+        mBandMembers.add(new BandMember(
+
+
+        ));
+
+        mBandMembers.add(new BandMember(
+
+
+        ));
+
+
+    }
 }
